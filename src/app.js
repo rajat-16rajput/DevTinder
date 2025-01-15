af7e0111c93e6962b1a3b1b7be06e5b91378dda0
rajat-16rajput <67589444+rajat-16rajput@users.noreply.github.com>
2025-01-15 23:54:59 +0530

    Created a server

---
+++ src/app.js
@@ -0,0 +1,27 @@
+//Importing Express Server
+const express = require("express");
+
+//Creating expres server
+const app = express();
+
+//Making the server listen on Port 7777
+app.listen(7777, () => {
+  console.log("Server listening to port 7777");
+});
+
+//Adding Request Handlers
+
+//Global Request handler - Adding this will shadow the other request handlers
+// app.use((req, res) => {
+//   res.send("Global HEllo WOrld");
+// });
+
+//Request handler for requests coming at path /hello
+app.use("/hello", (req, res) => {
+  res.send("Hello World from Hello");
+});
+
+//Request Handler for requests coming at the path /test
+app.use("/test", (req, res) => {
+  res.send("Hello From Test");
+});
