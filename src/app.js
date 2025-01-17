63bd09f15e3cd9358a861c896c4f5059f2d808a3
rajat-16rajput <67589444+rajat-16rajput@users.noreply.github.com>
2025-01-17 18:56:43 +0530

    Multiple Route Handlers, next()

--- src/app.js
+++ src/app.js
@@ -8,3 +8,28 @@ const app = express();
 app.listen(7777, () => {
   console.log("Server listening to port 7777");
 });
+
+//Note : For a specific route only one response is valid and is sent.
+//Adding multiple responses wont work since only the first response is sent due to synchronous behaviour of JS.
+app.use(
+  "/about",
+  (req, res, next) => {
+    next();
+  },
+  [
+    (req, res, next) => {
+      console.log("Array1");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Array2");
+      next();
+    }
+  ],
+  (req, res, next) => {
+    next();
+  },
+  (req, res) => {
+    res.send("Hello 4");
+  }
+);
