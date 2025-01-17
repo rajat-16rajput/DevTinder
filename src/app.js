b999061d2c7512b2725b0cc024acabb250cc1a9b
rajat-16rajput <67589444+rajat-16rajput@users.noreply.github.com>
2025-01-17 18:09:48 +0530

    Add different Routing paths, regex, query params and dyanamic routing

--- src/app.js
+++ src/app.js
@@ -11,6 +11,44 @@ app.listen(7777, () => {
 
 //Adding Request Handlers
 
+//Advanced Routes
+app.get("/(az)?b", (req, res) => {
+  res.send("THis is a?b");
+});
+
+app.get("/ab+c", (req, res) => {
+  res.send("/abbbbbbbbbbbc");
+});
+
+app.get("/a(bc)+d", (req, res) => {
+  res.send("abcbcbcbc");
+});
+
+app.get("/ab*d", (req, res) => {
+  res.send("/ab[*cvxdsdadsdsdaszdsdsdsasd]c");
+});
+
+//Regex route containing a
+/*app.get(/a/, (req, res) => {
+  res.send("Anyhing containing a");
+});*/
+
+//Regex route ending with fly
+app.get(/.*fly$/, (req, res) => {
+  res.send("Anything ending with fly");
+});
+
+//console.logging  query parameters
+app.get("/queryParams", (req, res) => {
+  console.log(req.query);
+  res.send("Query Parameter example");
+});
+
+app.get("/users/:id/:city", (req, res) => {
+  console.log(req.params);
+  res.send(req.params);
+});
+
 //Request handler for requests coming at path /hello
 app.get("/hello", (req, res) => {
   res.send({ firstName: "Rajatsingh", lasName: "Rajput" });
