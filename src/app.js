dbb54aafabdf033441047803e7e930b1d4681360
rajat-16rajput <67589444+rajat-16rajput@users.noreply.github.com>
2025-01-18 13:29:29 +0530

    Connecting to the mongodb server

--- src/app.js
+++ src/app.js
@@ -4,7 +4,17 @@ const express = require("express");
 //Creating expres server
 const app = express();
 
-//Making the server listen on Port 7777
-app.listen(7777, () => {
-  console.log("Server listening to port 7777");
-});
+//Importing ConnectToDB() from database.js
+
+const { ConnectToDB } = require("./Config/database");
+
+ConnectToDB()
+  .then(() => {
+    console.log("Successfully Connected to the database");
+    app.listen(7777, () => {
+      console.log("Server Listening to port 7777");
+    });
+  })
+  .catch((err) => {
+    console.error("Couldn't connect to the database");
+  });
