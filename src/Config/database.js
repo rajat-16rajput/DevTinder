8c4837b1e506c2a16c3cd6ee935178b3100bb4cb
rajat-16rajput <67589444+rajat-16rajput@users.noreply.github.com>
2025-01-18 17:55:23 +0530

    Add Data to the DB Collection

--- src/Config/database.js
+++ src/Config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function ConnectToDB() {
   await mongoose.connect(
-    "mongodb+srv://rajat:rajat123@cluster0.3fu6prp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/DevTinder"
+    "mongodb+srv://rajat:rajat123@cluster0.3fu6prp.mongodb.net/Avengers?retryWrites=true&w=majority&appName=Cluster0"
   );
 }
 
--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ const express = require("express");
 const app = express();
 
 //Importing ConnectToDB() from database.js
-
 const { ConnectToDB } = require("./Config/database");
+const User = require("./Models/user");
 
 ConnectToDB()
   .then(() => {
@@ -18,3 +18,21 @@ ConnectToDB()
   .catch((err) => {
     console.error("Couldn't connect to the database");
   });
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Steve",
+    lastName: "Rogers",
+    email: "Steve@Rogers.com"
+  });
+
+  try {
+    await user.save();
+    console.log("Added User data");
+    res.send("User Added Successfully !");
+  } catch (error) {
+    res
+      .status(400)
+      .send("There was an error while adding the user to the collection");
+  }
+});
